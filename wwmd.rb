219e6f877c28b85a097d6a550d87bc6cad88f739
Tim Lucas <t.lucas@toolmantim.com>
2008-12-05 09:40:13 +1100

    Make it a link for easy unlimited ismness

--- wwmd.rb
+++ wwmd.rb
@@ -19,9 +19,11 @@ __END__
     %meta{ :content => "text/html;charset=utf-8", "http-equiv" => "Content-Type" }/
     %title What would Myles do?
     %style{ :type => "text/css", :media => "screen" }
-      body{margin:0px;padding:0px;overflow:hidden;background:#FF0 url('/myles.jpg') center no-repeat;z-index:0;}
-      \#background{position:absolute;height:100%;width:100%;}
-      \#mism{overflow:auto;position:relative;height:100%;width:100%;left:0px;top:200px;color:white;text-align:center;margin:0 auto;padding:0 40px;font-family:Arial;font-size:72px;text-shadow:#000 2px 2px 2px;font-weight:bold;}
+      body{margin:0px; padding:0px; overflow:hidden; background:#FF0 url('/myles.jpg') center no-repeat; z-index:0;}
+      \#background{position:absolute; height:100%; width:100%;}
+      \#mism{display:block; overflow:auto; position:relative; height:100%; width:100%; left:0px; top:200px; color:white; text-align:center; margin:0 auto; padding:0 40px; font-family:Arial; font-size:72px; text-shadow:#000 2px 2px 2px; font-weight:bold;}
+      \a, a:visited, a:active, a:hover {color:white; text-decoration:none}
   %body
     #background
-    %p#mism= ism
+    %div
+      %a#mism{:href => "/"}= ism
