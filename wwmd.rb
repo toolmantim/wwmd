a65ebf971b21e37dcddec69fbc9f9ce6a82a5242
Chris Lloyd <christopher.lloyd@gmail.com>
2008-12-05 14:06:43 +1100

    WWMD V2.0

--- wwmd.rb
+++ wwmd.rb
@@ -17,32 +17,47 @@ __END__
 %html{ "xml:lang" => "en", :lang => "en", :xmlns => "http://www.w3.org/1999/xhtml" }
   %head
     %meta{ :content => "text/html;charset=utf-8", "http-equiv" => "Content-Type" }/
-    %title What would Myles do?
+    %title What Would Myles Do?
     %style{ :type => "text/css", :media => "screen" }
       :sass
         *
           :margin 0
           :padding 0
-        body
-          :overflow hidden
-          :background #FF0 url("/myles.jpg") center no-repeat
-          :z-index 0
-          :position relative
-          :height 100%
-        h1
-          :position static
-          :margin-top 2em
-          
-          :line-height 1em
-          :color white
-          :text-align center
-          :font-family Arial, sans-serif
-          :font-size 72px
-          :text-shadow #000 0 2px
-          :font-weight bold
-        a, a:visited, a:active
-          :color white
-          :text-decoration none
+        
+        html
+          :background #6F9
+        
+        #content
+          :background #9CF url('/images/background.png') no-repeat -200px 0
+          h1
+            :margin-left 120px
+            :padding-top 40px
+          #background
+            :background url('/images/footer.png') no-repeat 0 0
+            :height 407px
+          #container
+            :position absolute
+            :display table
+            :top 150px
+            :left 250px
+            #bubble
+              :height 264px
+              :width 441px
+              :vertical-align middle
+              :display table-cell
+              
+              :background url('/images/speech-bubble.png') no-repeat
+              :font-family Arial, sans-serif
+              :font-size 30px
+              :font-weight bold
+              :text-align center
+              :color #333
+              :padding 0 70px
   %body
-    %h1
-      %a{:href => "/"}= ism
+    #content
+      %h1
+        %img{ :src => '/images/what-would-myles-do.png' }
+      #background
+      #container
+        #bubble
+          %a{:href => "/"}= ism
